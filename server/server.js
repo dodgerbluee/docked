d9260ff4afc37c21abffb225b01c39f8d97ff6d1
Greg Sanders <gregasanders91@gmail.com>
2025-11-07 17:15:02 -0600

    feat: 0.1 release

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 // Serve static files from React app (in production)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../public")));
+  app.use(express.static(path.join(__dirname, "public")));
 }
 
 // Routes
@@ -27,7 +27,7 @@ app.use("/api", routes);
 // Serve React app for all non-API routes (in production)
 if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
   });
 }
 
